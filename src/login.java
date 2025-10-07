122382f59059b8fc1b02d8e7333d0a7cb5a22d4b
Brisa Ledezma <brisaledezma000@gmail.com>
2025-10-07 19:15:24 -0300

    Empezando con las vistas

---
+++ src/login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class login {
+    private JLabel usuario;
+    private JTextField txtUsuario;
+    private JLabel Contraseña;
+    private JPasswordField txtpassword;
+    private JCheckBox recordameCheckBox;
+    private JButton ingresarButton;
+}
---
+++ src/principal.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+public class principal {
+    private JTextField dniTxt;
+    private JButton buscarButton;
+    private JLabel dni;
+    private JButton cobrarButton;
+    private JButton nuevaVentaButton;
+    private JButton cerrarSesionButton;
+    private JButton volverButton;
+    private JPanel BorderLayout;
+    private JPanel panelBarra;
+    private JList menuList;
+    private JScrollPane barraPanel;
+    private JPanel panelContenido;
+    private JPanel cardSocios;
+    private JPanel cardMembresia;
+    private JPanel cardProductos;
+    private JPanel cardEjercicios;
+    private JPanel cardRutinas;
+    private JPanel cardRegistros;
+    private JPanel cardReportes;
+    private JButton agregarSocioButton;
+    private JButton modificarSocioButton;
+    private JButton eliminarSocioButton;
+    private JTable table1;
+    private JPanel panelSocios;
+    private JButton nuevaMembresiaButton;
+    private JButton modificarMembresiaButton;
+    private JButton eliminarMembresiaButton;
+    private JTable table2;
+    private JTextField txtInicio;
+    private JComboBox comboBox1;
+    private JButton nuevoAlumnoButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
